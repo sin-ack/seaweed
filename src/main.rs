5627ac9f7b84b1d869cec1207f77128a8eef7287
sin-ack <sin-ack@protonmail.com>
2025-08-19 23:18:28 +0000

    Make CLI accept stdin as input

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
+use std::io::Read;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// The expression to tokenize
-    #[arg(short = 'x', long)]
-    expression: String,
+    /// The expression to tokenize. If not provided, stdin will be used as input.
+    #[arg(short = 'x', long, required = false)]
+    expression: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let mut tokenizer = seaweed::Tokenizer::new(&args.expression);
+    let expression = match args.expression {
+        Some(expr) => expr,
+        None => {
+            let mut buffer = Vec::new();
+            std::io::stdin()
+                .read_to_end(&mut buffer)
+                .expect("Failed to read from stdin");
+            String::from_utf8(buffer).expect("Invalid UTF-8 input")
+        }
+    };
+
+    let mut tokenizer = seaweed::Tokenizer::new(&expression);
 
     loop {
         match tokenizer.next_token() {
